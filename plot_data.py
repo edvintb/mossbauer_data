c63112c01802981b60cb47384c6434491911c96f
Edvin T. Berhane <edvinb33@gmail.com>
2022-11-24 10:04:35 -0500

    added chi2

--- plot_data.py
+++ plot_data.py
@@ -3,24 +3,26 @@ import matplotlib.pyplot as plt
 import os
 from scipy import optimize
 from scope_fit_11_22 import scope_fit
+import re
 
 # Get all files in folder
 folder_name = "FeF2_84.6K/"
 file_ending = '.txt'
 file_list = [file for file in os.listdir(folder_name) if file.endswith(file_ending)]
 
-# Take care of weird last file
-highest_numbered_file = file_list.pop()
+# Get data from highest numbered file
+highest_numbered_file = sorted(file_list, key=lambda file: int(re.findall('[0-9]+.txt', file)[0][:-4])).pop()
+print(highest_numbered_file)
 time_arr, total_count_arr = np.loadtxt(folder_name + highest_numbered_file, delimiter='\t', skiprows=5, unpack=True)
 
 # Aggregate data from all the files
-for file in file_list:
-  count_array = np.loadtxt(folder_name + file, delimiter='\t', skiprows=5, usecols=2, unpack=True)
-  total_count_arr += count_array
+# for file in file_list:
+#   count_array = np.loadtxt(folder_name + file, delimiter='\t', skiprows=5, usecols=2, unpack=True)
+#   total_count_arr += count_array
 
 # Plot data to check where fits need to happen
 fig1, ax1 = plt.subplots()
-ax1.plot(time_arr, total_count_arr, label='Raw counts')
+ax1.scatter(time_arr, total_count_arr, label='Raw counts', linewidths=0.5)
 ax1.set_title('Counts vs. Time')
 ax1.set_yscale("linear")
 ax1.set_xscale("linear")
@@ -35,29 +37,40 @@ fig1.show()
 # plt.close('all')
 # exit()
 
-# Define Lorentzian to fit
-def lorentzian(data, E0, amplitude, Gamma, offset):
-  return amplitude * (Gamma / ((data - E0)**2 + (Gamma / 2)**2)) + offset
-
 # Define peaks and their approximate positions
 number_peaks = 4
+fit_range = np.asarray([(0.0025, 0.0075), (0.0030, 0.0080), (0.0150, 0.0200), (0.0150, 0.0200)])
 approximate_positions = np.asarray([0.005, 0.006, 0.0174, 0.0180])
 approximate_amplitude = np.asarray([-20000, -20000, -20000, -20000])
 approximate_width = np.asarray([0.0005, 0.0005, 0.0005, 0.0005])
-approximate_offset = 95000 * np.ones(number_peaks)
-parameter_matrix = np.transpose(np.vstack((approximate_positions, approximate_amplitude, approximate_width, approximate_offset)))
+approximate_offset = 14500 * np.ones(number_peaks)
+param_matrix = np.transpose(np.vstack((approximate_positions, approximate_amplitude, approximate_width, approximate_offset)))
 
-opt_param_matrix = np.zeros(np.shape(parameter_matrix))
+# Define and fit Lorentzian
+def lorentzian(data, E0, amplitude, Gamma, offset):
+  return amplitude * (Gamma / ((data - E0)**2 + (Gamma / 2)**2)) + offset
 
+opt_param_matrix = np.zeros(np.shape(param_matrix))
+cov_matrix = np.zeros(shape=(np.shape(param_matrix)[0], np.shape(param_matrix)[1], np.shape(param_matrix)[1]))
+chi2_matrix = np.zeros(shape=(number_peaks, 2))
 for index in range(number_peaks):
-  opt_param_matrix[index], _ = optimize.curve_fit(lorentzian, time_arr, total_count_arr, parameter_matrix[index])
+  time_to_fit = [time for time in time_arr if time >= fit_range[index][0] and time <= fit_range[index][1]]
+  start_index = np.where(time_arr == time_to_fit[0])[0][0]
+  stop_index =  np.where(time_arr == time_to_fit[-1])[0][0] + 1
+  counts_to_fit = total_count_arr[start_index:stop_index]
+  count_std_dev = np.sqrt(counts_to_fit) # Poission process has variance lambda
+  opt_param_matrix[index], cov_matrix[index] = optimize.curve_fit(lorentzian, time_to_fit, counts_to_fit, param_matrix[index], sigma=count_std_dev)
+  chi2 = np.sum(np.square(np.divide(np.subtract(counts_to_fit, lorentzian(time_to_fit, *opt_param_matrix[index])), count_std_dev)))
+  deg_of_freedom = len(time_to_fit) - np.shape(param_matrix)[1]
+  reduced_chi2 = chi2 / deg_of_freedom
+  chi2_matrix[index] = [chi2, reduced_chi2]
 
 # Plot data to check fits
 fig2, ax2 = plt.subplots()
-ax2.plot(time_arr, total_count_arr, label='Raw counts')
+ax2.errorbar(time_arr, total_count_arr, yerr = np.sqrt(total_count_arr), capsize=2, label='Count Data', fmt='.')
 time_points = np.linspace(min(time_arr), max(time_arr), 10000)
 for index in range(number_peaks):
-  ax2.plot(time_points, lorentzian(time_points, *opt_param_matrix[index]), label=index)
+  ax2.plot(time_points, lorentzian(time_points, *opt_param_matrix[index]), label=r'$\chi^2$: {:.2g}, $\chi^2_{{\nu}}$: {:.2g}'.format(*chi2_matrix[index]))
 ax2.set_title('Counts vs. Time')
 ax2.set_yscale("linear")
 ax2.set_xscale("linear")
@@ -65,9 +78,8 @@ ax2.set_xlabel('Time')
 ax2.set_ylabel('# Counts')
 ax2.legend(framealpha=1, shadow=True, loc='best')
 ax2.grid(alpha=0.25)
-# Save .png of plot to folder with file
 fig2.savefig('{}/{}_counts_time_fit.png'.format(folder_name[:-1], folder_name[:-1]))
-# fig2.show()
+fig2.show()
 # input("hit[enter] to close all plots")
 # plt.close('all')
 
@@ -75,14 +87,14 @@ fig2.savefig('{}/{}_counts_time_fit.png'.format(folder_name[:-1], folder_name[:-
 num_distinct_peaks = int(number_peaks/2)
 amp, ang_freq, phase_transducer, shift = scope_fit.cos_params_from_data()
 velocity_voltage_ratio=0.080523086265892;
-peak_velocity_matrix = np.zeros((num_distinct_peaks, 2))
+peak_velocity_matrix = np.zeros((num_distinct_peaks, 3))
 for index in range(num_distinct_peaks):
   left_peak_time = opt_param_matrix[index][0]
   right_peak_time = opt_param_matrix[-index][0]
   time_diff = right_peak_time - left_peak_time
   peak_voltage = scope_fit.voltage_from_time_diff(time_diff, amp, ang_freq)
   peak_velocity = peak_voltage * velocity_voltage_ratio
-  peak_velocity_matrix[index] = [left_peak_time, peak_velocity]
+  peak_velocity_matrix[index] = [left_peak_time, peak_velocity, right_peak_time]
 
 # Fit the phase of trig function to these velocities
 def velocity(time, phase_velocity):
@@ -90,13 +102,19 @@ def velocity(time, phase_velocity):
 
 phase_change, _ = optimize.curve_fit(velocity, peak_velocity_matrix[:, 0], peak_velocity_matrix[:, 1])
 
-print('Phase change: {}'.format(phase_change[0]))
+print('Phase change: {}'.format(*phase_change))
+print('Velocity at time 0: {}'.format(velocity(0, *phase_change)))
+print(peak_velocity_matrix[0, 1])
+print(peak_velocity_matrix[1, 1])
 
 # Check fit to these points
 fig3, ax3 = plt.subplots()
 ax3.scatter(peak_velocity_matrix[:, 0], peak_velocity_matrix[:, 1], label='Velocity set by diff')
 time_points = np.linspace(min(time_arr), max(time_arr), 10000)
-ax3.plot(time_points, velocity(time_points, phase_change[0]), label='Fitted velocity trig', color='orange')
+ax3.plot(time_points, velocity(time_points, *phase_change), label='Fitted velocity trig', color='orange')
+# for index in range(num_distinct_peaks):
+# ax3.hlines(peak_velocity_matrix[0, 1], peak_velocity_matrix[0, 0], peak_velocity_matrix[0, 2], ls='dashed', label='Diff line 1')
+# ax3.hlines(peak_velocity_matrix[1, 1], peak_velocity_matrix[1, 0], peak_velocity_matrix[1, 2], ls='dashed', label='Diff line 2', color='red', lw=10)
 ax3.set_title('Velocity vs. Time')
 ax3.set_yscale("linear")
 ax3.set_xscale("linear")
@@ -109,7 +127,6 @@ fig3.savefig('{}/{}_vel_phase_check.png'.format(folder_name[:-1], folder_name[:-
 fig3.show()
 input("hit[enter] to close all plots")
 plt.close('all')
-
 
 exit()
 
